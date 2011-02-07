943bb3cf571230a893bc7738dbcb97b13c64e7e4
Jon Boekenoogen <metalhead8815@gmail.com>
2011-02-06 22:44:34 -0800

    Add bots to database so others do not get those ids. Remove game.sql since it is not needed.

--- CardGameServer/schema/users.sql
+++ CardGameServer/schema/users.sql
@@ -1,4 +1,4 @@
-DROP TABLE users IF EXISTS users;
+DROP TABLE IF EXISTS users;
 
 CREATE TABLE users
  (
@@ -11,3 +11,15 @@ CREATE TABLE users
  
 CREATE UNIQUE INDEX user_name_index ON users (user_name);
 CREATE UNIQUE INDEX email_index ON users (email);
+
+INSERT INTO users(id, user_name, email, password, state) VALUES(1, 'Bot1', 'bot1@fakemail.com', '', 0);
+INSERT INTO users(id, user_name, email, password, state) VALUES(2, 'Bot2', 'bot2@fakemail.com', '', 0);
+INSERT INTO users(id, user_name, email, password, state) VALUES(3, 'Bot3', 'bot3@fakemail.com', '', 0);
+INSERT INTO users(id, user_name, email, password, state) VALUES(4, 'Bot4', 'bot4@fakemail.com', '', 0);
+INSERT INTO users(id, user_name, email, password, state) VALUES(5, 'Bot5', 'bot5@fakemail.com', '', 0);
+INSERT INTO users(id, user_name, email, password, state) VALUES(6, 'Bot6', 'bot6@fakemail.com', '', 0);
+INSERT INTO users(id, user_name, email, password, state) VALUES(7, 'Bot7', 'bot7@fakemail.com', '', 0);
+INSERT INTO users(id, user_name, email, password, state) VALUES(8, 'Bot8', 'bot8@fakemail.com', '', 0);
+INSERT INTO users(id, user_name, email, password, state) VALUES(9, 'Bot9', 'bot9@fakemail.com', '', 0);
+INSERT INTO users(id, user_name, email, password, state) VALUES(10, 'Bot10', 'bot10@fakemail.com', '', 0);
+
